1e5b8902377cb73f200ec0b1a74495b75eceedec
Nikhil <115878497+Nikhilkb2255@users.noreply.github.com>
2023-11-20 10:57:52 +0530

    Add files via upload

---
+++ q1/MainActivity.java
@@ -0,0 +1,30 @@
+//package com.example.q1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.q1.R;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button clickme = findViewById(R.id.b1);
+        clickme.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+            Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+        }
+    });
+    }
+}
